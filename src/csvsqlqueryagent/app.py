c14b8255c697a51c8a2315f5bcb672d34956e6bc
chaminda satanarachchi <chami.7896@gmail.com>
2025-03-04 13:56:52 +0530

    add .gitkeep in data and db folder modify app.py for clear the path of application

--- src/csvsqlqueryagent/app.py
+++ src/csvsqlqueryagent/app.py
@@ -20,14 +20,17 @@ from langchain_community.utilities import SQLDatabase
 
 # create a db from csv file
 
+# Define paths relative to the project root
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # Moves up three levels
+
 # Path to your SQLite database file
-database_file_path = "./db/salary.db"
+database_file_path = os.path.join(BASE_DIR, "db", "salary.db")
 
 
 # Create an engine to connect to the SQLite database
 # SQLite only requires the path to the database file
 engine = create_engine(f"sqlite:///{database_file_path}")
-file_url = "./data/salaries_2023.csv"
+file_url = os.path.join(BASE_DIR, "data", "salaries_2023.csv")
 os.makedirs(os.path.dirname(database_file_path), exist_ok=True)
 df = pd.read_csv(file_url).fillna(value=0)
 df.to_sql("salaries_2023", con=engine, if_exists="replace", index=False)
